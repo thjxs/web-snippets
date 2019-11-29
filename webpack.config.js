49004ba08e4e6c378748abf6420beaff5fd69609
thjxs <tanhongjianx@163.com>
2019-11-29 18:10:57 +0800

    Add lazy load

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path')
 
-module.exports = {
+module.exports = [
+{
     mode: 'production',
     entry: './bootstrap.js',
     output: {
         path: path.resolve(__dirname, './'),
         filename: 'index.js'
     }
+},
+{
+    mode: 'production',
+    entry: './src/random.js',
+    output: {
+        path: path.resolve(__dirname, './'),
+        filename: 'random.js'
+    }
 }
+]
