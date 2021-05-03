e93294e70a1a30f56b1a04c589551a9a386eec88
thjxs <tanhongjian@outlook.com>
2021-05-04 01:44:26 +0800

    Add pseudo

---
+++ src/pages/Pseudo.tsx
@@ -0,0 +1,12 @@
+import './Pseudo.css';
+
+export default function Pseudo() {
+  return (
+    <section>
+      <p>
+        This is content text that you can read from <a href="#1">hehehehe</a>,
+        or from another one <a href="#2">biubiubiu</a>
+      </p>
+    </section>
+  );
+}
